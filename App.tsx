2d7f2410994951708acaca0fe835c2b31b8fb199
JANGTAEHEE <jangth0655@gmail.com>
2022-05-18 21:58:15 +0900

    setup expo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        const fontsToLoad = [Ionicons.font];
+        const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+        const imageToLoad = [require("./assets/favicon.png")];
+        const imagePromises = imageToLoad.map((image) =>
+          Asset.loadAsync(image)
+        );
+        console.log(imagePromises);
+        return Promise.all([...imagePromises, ...fontPromises]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text>Hello!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
